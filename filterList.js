889c405a16c02218d19cf4e1037b1407fcb08a74
benkrzanowski <benkrzanowski@yahoo.com>
2019-03-29 00:09:38 -0500

    first commit

---
+++ filterList.js
@@ -0,0 +1,7 @@
+function filter_list(l) {
+  return l.filter(el => typeof el === "number");
+}
+
+filter_list([1, 2, "a", "b"]);
+filter_list([1, "a", "b", 0, 15]);
+filter_list([1, 2, "aasf", "1", "123", 123]);
